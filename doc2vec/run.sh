a2ac0e282990694c58c3b0bb30fe9e2564c9c96e
jalali <azam.jalali@gmail.com>
2019-12-08 17:48:16 +0330

    correct path input file in run.sh

--- doc2vec/run.sh
+++ doc2vec/run.sh
@@ -2,7 +2,7 @@
 mkdir -p input_data/emails
 echo running utils.py
 python -u utils.py
-cd input_data
+cd input_data/emails
 mkdir sample
 ls emails | sort -R | tail -1000 | xargs -I % cp emails/% sample # generate a random sample of 1000
 export DOC_TO_TEST=`ls sample | head -1` # example: enron001_01084.txt
