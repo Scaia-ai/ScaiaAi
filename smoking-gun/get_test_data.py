07d3d0b434b5110b4be176b380ce14ec8e756a70
jalali <azam.jalali@gmail.com>
2019-12-10 20:52:42 +0330

    change mode

--- smoking-gun/get_test_data.py
+++ smoking-gun/get_test_data.py
@@ -22,14 +22,14 @@ def clone_test_data(download_to_folder = os.path.join("input_data", "emails")):
     for item in os.listdir(source):
         s = source / item
         d = destination / item
-        #os.chmod(s, 775)
+        #os.chmod(s, 664)
         if s.is_dir():
             copy_dir(s, d)
         else:
             shutil.copyfile(s, d)
+            os.chmod(d, 664)
     return
-    for f in os.listdir(destination):
-         os.chmod(f, 664)
+
 
 
 if __name__ == "__main__":
