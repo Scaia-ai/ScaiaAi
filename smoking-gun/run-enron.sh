7c8e29f2a6bf2855c6f566d8a5d86d860a7e23ed
Matthew Danielson <danielsonmatthew4@gmail.com>
2019-12-31 10:16:29 -0600

    Removed older files for updates

--- smoking-gun/run-enron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p input_data/emails
-mkdir output_data
-python -u get_enron.py
-python process_data.py
-python document_representations_bert.py
-python bert_embeddings.py
-python -u Ai-Advisor-enron.py
--- smoking-gun/run-freed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p input_data/results/text
-mkdir output_data
-python -u get_freed.py
-python process_data.py
-python document_representations_bert.py
-python bert_embeddings.py
-python -u Ai-Advisor-freed.py
