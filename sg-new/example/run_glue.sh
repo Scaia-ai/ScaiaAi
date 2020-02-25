9c86dccfef4cc6f3273d10b137803d92b481b5e9
Tim Fox <tim@elephantscale.com>
2020-02-25 09:01:03 -0500

    Added glue example

---
+++ sg-new/example/run_glue.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export TASK_NAME=STS-B 
+export GLUE_DIR=$SCAIA_HOME/datasets/glue/glue_data 
+export CUDA_VISIBLE_DEVICES=0 
+python run_glue.py   --model_type bert   --model_name_or_path bert-base-cased   --task_name $TASK_NAME   --do_train   --do_eval   --do_lower_case   --data_dir $GLUE_DIR/$TASK_NAME   --max_seq_length 128   --per_gpu_train_batch_size 32   --learning_rate 2e-5   --num_train_epochs 3.0   --output_dir /tmp/$TASK_NAME/
