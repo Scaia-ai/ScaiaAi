ef5cf920392388d7a23335c841325e05b962be4d
Tim Fox <tim@elephantscale.com>
2020-10-24 09:29:10 -0400

    Added pegasus huggingface

---
+++ pegasus/huggingface/hf_pegasus.py
@@ -0,0 +1,15 @@
+import sys
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
+import torch
+src_text = [
+    """ PG&E stated it scheduled the blackouts in response to forecasts for high winds amid dry conditions. The aim is to reduce the risk of wildfires. Nearly 800 thousand customers were scheduled to be affected by the shutoffs which were expected to last through at least midday tomorrow."""
+]
+
+model_name = 'google/pegasus-xsum'
+torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = PegasusTokenizer.from_pretrained(model_name)
+model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
+batch = tokenizer.prepare_seq2seq_batch(src_text, truncation=True, padding='longest').to(torch_device)
+translated = model.generate(**batch)
+tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+print(tgt_text)
